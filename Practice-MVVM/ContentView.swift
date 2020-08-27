7a7aef2ffa6831e85ad7cae1162c2a701d1392b7
Gerson Janhuel <gersonjanhuelhuel@gmail.com>
2020-08-27 15:32:50 +0700

    Initial Commit

---
+++ Practice-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practice-MVVM
+//
+//  Created by Gerson Janhuel on 27/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Practice-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice-MVVM
+//
+//  Created by Gerson Janhuel on 27/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
